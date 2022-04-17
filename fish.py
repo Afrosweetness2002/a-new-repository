4bdbb9b34d00142cf5922233ee74e3ef4908dc3c
Abdullatif <abdullatiffarah21@gmail.com>
2022-04-17 19:30:08 +0100

    This is my third Git save

---
+++ fish.py
@@ -0,0 +1,8 @@
+def main():
+    #This line of code is where I call try
+    try:
+        print(input(": "))
+    except KeyboardInterrupt:
+        print("So long, and thanks for all the fish")
+
+main()    
