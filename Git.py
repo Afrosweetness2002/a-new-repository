1efd6629adcd45ac932a03814d000f7c3fe2d100
Abdullatif <abdullatiffarah21@gmail.com>
2022-04-18 11:00:32 +0100

    please just pull up to the GitHub! Git.py!

---
+++ Git.py
@@ -0,0 +1,9 @@
+print("Hello Git Hub This is me practicing how to create a repository")
+
+def hello(n):
+    dope = "n"
+    return dope
+
+print(" now checking to see if it's tracking ")
+
+print(" you better be uploaded on the Github!!")
